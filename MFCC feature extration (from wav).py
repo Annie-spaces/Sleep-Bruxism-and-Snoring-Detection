033ce5febf14c587ab25c5d4c9f37478348bec58
Annie-spaces <huangwanqi@u.nus.edu>
2025-02-14 11:00:28 +0800

    read in multiple online dataset, generate mfcc array

--- MFCC feature extration (from wav).py
+++ MFCC feature extration (from wav).py
@@ -38,20 +38,41 @@ import requests
 import librosa
 import io
 import soundfile as sf
-wav_url = "https://raw.githubusercontent.com/adrianagaler/Snoring-Detection/master/Snoring_Dataset_%4016000/snoring/1_0.wav"
-response = requests.get(wav_url)
-if response.status_code == 200:
-    y, sr = sf.read(io.BytesIO(response.content))  # Read the WAV file
-    y_new = librosa.resample(y, orig_sr=sr, target_sr=8000) #resampled y
-    sr_new=8000 #new sampling rate
-    print("Original Audio Shape:", y.shape)
-    print("Resampled Audio Shape:", y_new.shape)
-else:
-    raise Exception("Failed to download the audio file")
-mfccs = librosa.feature.mfcc(y=y_new, sr=sr_new, n_mfcc=13)
-print(mfccs)
-print("MFCC Shape:", mfccs.shape)
+# wav_url = "https://raw.githubusercontent.com/adrianagaler/Snoring-Detection/master/Snoring_Dataset_%4016000/snoring/1_0.wav"
+# response = requests.get(wav_url)
+# if response.status_code == 200:
+#     y, sr = sf.read(io.BytesIO(response.content))  # Read the WAV file
+#     y_new = librosa.resample(y, orig_sr=sr, target_sr=8000) #resampled y
+#     sr_new=8000 #new sampling rate
+#     print("Original Audio Shape:", y.shape)
+#     print("Resampled Audio Shape:", y_new.shape)
+# else:
+#     raise Exception("Failed to download the audio file")
+# mfccs = librosa.feature.mfcc(y=y_new, sr=sr_new, n_mfcc=13)
+# print(mfccs)
+# print("MFCC Shape:", mfccs.shape)
 
+#version 4 reading multiple online url, getting average mfcc for each sample, store them in a numpy array (column is features, row is data)
+import numpy as np
+MFCC_list = np.empty((0, 13))
+for i in range(1, 10):
+    url = "https://raw.githubusercontent.com/adrianagaler/Snoring-Detection/master/Snoring_Dataset_%4016000/snoring/1_" + str(i) + ".wav"
+    response = requests.get(url)
+    print('now processing', i, '.wav:')
+    print('response', response)
+    if response.status_code == 200:
+        y, sr = sf.read(io.BytesIO(response.content))  # Read the WAV file
+        y_new = librosa.resample(y, orig_sr=sr, target_sr=8000)  # resampled y
+        sr_new = 8000  # new sampling rate
+    else:
+        raise Exception("Failed to download the audio file")
+    mfccs = librosa.feature.mfcc(y=y_new, sr=sr_new, n_mfcc=13)
+    print('shape', i,'mfcc:', mfccs.shape)
+    average_mfcc = np.mean(mfccs, axis=1)
+    print('for', i, '.wav, average mfcc is', average_mfcc)
+    MFCC_list=np.vstack([MFCC_list, average_mfcc])
+print(MFCC_list)
+print("MFCC Shape:",MFCC_list.shape)
 
 
 
